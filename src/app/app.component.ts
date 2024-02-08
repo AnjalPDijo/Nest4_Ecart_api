03c3f9a06cb833f0638fe9816f726cb3226fe68e
AnjalPDijo <anjalpdijo24@gmail.com>
2024-02-08 16:23:37 +0530

    Ecart with Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+import { SlicePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SlicePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ECart';
+data: any=[]
+slice: any;
+constructor(public api:ApiService){}
+ngOnInit()
+{
+  this.api.getdata().subscribe(response=>{
+    this.data=response
+  })
+}
 }
